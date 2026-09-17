f8a00e32d4e153e1c29fdc881a688e2e151b62b0
Vishnu <u.vishnu3568@gmail.com>
2026-09-17 11:17:51 +0530

    test(scientific): update customer FAISS vector assertion in exploration test (R-03)

--- tests/test_scientific_exploration.py
+++ tests/test_scientific_exploration.py
@@ -341,7 +341,7 @@ class TestScientificExploration(unittest.TestCase):
 
         import faiss
         c_idx = faiss.read_index(str(customer_faiss / "index.faiss"))
-        self.assertEqual(c_idx.ntotal, 79)
+        self.assertEqual(c_idx.ntotal, 76)
 
         s_idx = faiss.read_index(str(self.scientific_store_path / "index.faiss"))
         self.assertEqual(s_idx.ntotal, 100)
